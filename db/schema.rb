e87fdbb3a1e3b0cf89dac95da7ef7428bbc07563
biboyatienza <biboyatienza@gmail.com>
2014-05-27 00:35:36 +0800

    redo games table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526143240) do
+ActiveRecord::Schema.define(version: 20140526162347) do
 
   create_table "combinations", force: true do |t|
     t.integer  "winning_number"
